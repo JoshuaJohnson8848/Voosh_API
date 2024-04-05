a11b448c15882596b0f9640564d0ca2c189043ca
JoshuaJohnson8848 <joshuatjohnson255@gmail.com>
2024-04-06 00:23:27 +0530

    Added login

--- router/Auth/auth.js
+++ router/Auth/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const authController = require('../../controller/Auth/auth');
 
+router.post('', authController.login);
+
 router.post('/signup', authController.signup);
 
 module.exports = router;
